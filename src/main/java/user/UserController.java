6e3a506bf7503d1844e8f1cdbd761d413f52c891
Luna <pablo.luna@solera.com>
2022-11-28 16:03:22 +0100

    second

--- src/main/java/user/UserController.java
+++ src/main/java/user/UserController.java
@@ -1,5 +1,20 @@
 package user;
 
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class UserController {
 
+
+	private UserService userService;
+	
+	
+	public UserController(String username, String password) {
+		this.userService = new UserService(username, password);
+	}
+	
+	
+	
+	
+	
 }
